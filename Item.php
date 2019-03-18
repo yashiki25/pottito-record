432ef71e44ad0ce4718b0c057bd590a92ae645e4
vagrant <vagrant@localhost.localdomain>
2019-03-18 12:19:20 +0900

    PHPによるDB連携の実装

---
+++ Item.php
@@ -0,0 +1,115 @@
+<?php
+
+class Item {
+  private $_db;
+
+  public function __construct() {
+    $this->_connectDB();
+    $this->_createToken();
+  }
+
+  private function _connectDB() {
+    try {
+      $this->_db = new \PDO(DSN, DB_USERNAME, DB_PASSWORD);
+      $this->_db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    } catch (\PDOException $e) {
+      echo $e->getMessage();
+      exit;
+    }
+  }
+
+  private function _createToken() {
+    if (!isset($_SESSION['token'])) {
+      $_SESSION['token'] = bin2hex(openssl_random_pseudo_bytes(16));
+    }
+  }
+
+  public function getAll() {
+    $stmt = $this->_db->query('select * from items order by id asc');
+    return $stmt->fetchAll(\PDO::FETCH_OBJ);  // オブジェクト形式で返す
+  }
+
+  public function post() {
+    try {
+      $this->_validateToken();
+      $this->_validateItem();
+      $this->_save();
+      header('Location: http://' . $_SERVER['HTTP_HOST'] . '/index.php');
+    } catch (\Exception $e) {
+      $_SESSION['err'] = $e->getMessage();
+    }
+  }
+
+  public function getError() {
+    $err = null;
+
+    if(isset($_SESSION['err'])) {
+      $err = $_SESSION['err'];
+      unset($_SESSION['err']);
+    }
+
+    return $err;
+  }
+
+  private function _validateToken() {
+    if (
+      !isset($_SESSION['token']) ||
+      !isset($_POST['token']) ||
+      $_SESSION['token'] !== $_POST['token']
+    ) {
+      throw new \Exception('invalid token!');
+    }
+  }
+
+  private function _validateItem() {
+    if(!isset($_POST['add-name']) ||
+    !strlen($_POST['add-name']) ||
+    !isset($_POST['add-price']) ||
+    !strlen($_POST['add-price'])
+    ) {
+      throw new \Exception('invalid input!');
+    }
+  }
+
+  private function _save() {
+    $sql = 'insert into items (name, price) values (:name, :price)';
+    $stmt = $this->_db->prepare($sql);
+    $stmt->bindValue(':name', $_POST['add-name'], \PDO::PARAM_STR);
+    $stmt->bindValue(':price', $_POST['add-price'], \PDO::PARAM_INT);
+    $stmt->execute();
+  }
+
+  public function update() {
+    try {
+      $this->_update();
+      header('Location: http://' . $_SERVER['HTTP_HOST'] . '/index.php');
+    } catch (\Exception $e) {
+      $_SESSION['err'] = $e->getMessage();
+    }
+  }
+
+  private function _update() {
+    foreach($_POST['edit'] as $item) {
+      // 削除
+      if($item['editDelete'] == 'true') {
+          $this->_delete($item);
+          continue;
+      }
+
+      // 品名・価格の更新
+      $sql = 'update items set name = :name, price = :price where id = :id';
+      $stmt = $this->_db->prepare($sql);
+      $stmt->bindValue(':name', $item['editName'], \PDO::PARAM_STR);
+      $stmt->bindValue(':price', (int)$item['editPrice'], \PDO::PARAM_INT);
+      $stmt->bindValue(':id', (int)$item['editId'], \PDO::PARAM_INT);
+      $stmt->execute();
+    }
+  }
+
+  private function _delete($item) {
+    $sql = 'delete from items where id = :id';
+    $stmt = $this->_db->prepare($sql);
+    $stmt->bindValue(':id', (int)$item['editId'], \PDO::PARAM_INT);
+    $stmt->execute();
+  }
+}
---
+++ index.php
@@ -0,0 +1,175 @@
+<?php
+require_once(__DIR__ . '/config.php');
+require_once(__DIR__ . '/functions.php');
+require_once(__DIR__ . '/Item.php');
+require_once(__DIR__ . '/purchaseItem.php');
+
+// 購入品リストを取得
+try{
+  $itemList = new \Item();
+  $items = $itemList->getAll();
+
+  $purchaseHistory = new \PurchaseHistory();
+  $purchaseItems = $purchaseHistory->getAll();
+} catch (Exception $e) {
+  echo $e->getMessage();
+}
+
+// 購入品リストの追加
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['add-name'])) {
+  $itemList->post();
+  $err = $itemList->getError();
+}
+
+// 購入履歴の登録
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['item'])) {
+  $purchaseHistory->post($_POST['item']);
+  $err = $purchaseHistory->getError();
+}
+
+// 購入履歴の削除
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete'])) {
+  $purchaseHistory->delete($_POST['delete']);
+  $err = $purchaseHistory->getError();
+}
+
+// 購入品リストの編集
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['edit'])) {
+  $itemList->update();
+  $err = $itemList->getError();
+}
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="ぽちっと簡単登録！n日坊主になるための家計簿もどき">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>ぽちっと家計簿</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+      integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+
+  <body>
+    <header>
+      <h1>ぽちっとかけいぼ</h1>
+      <p>気軽にぽちっと家計簿つけよう</p>
+      <time></time>
+    </header>
+    <section class="total">
+      <div>
+        今月の合計
+        <span id="total-price">0</span>
+      </div>
+    </section>
+    
+    <section class="graph">
+      <div id="open">
+        <i class="fas fa-chart-pie"></i> レポートを見る
+      </div>
+    
+      <div id="mask" class="hidden"></div>
+    
+      <div id="modal" class="hidden">
+        <h3>内訳</h3>
+        <table></table>
+        <h3>支出グラフ</h3>
+        <div id="target"></div>
+        <div id="close">
+          <i class="fas fa-window-close"> 閉じる</i>
+        </div>
+      </div>
+    </section>
+    
+    <section class="item-list">
+      <h2><i class="fas fa-pen-nib"></i> 購入品をぽちっと登録</h2>
+      <button id="edit">編集</button>
+
+      <!-- 購入リストが登録されている場合に表示 -->
+      <p id="item-list-info">「編集」から購入品リストを作成してください。</p>
+
+      <form id="item-list-from" action="" method="POST">
+        <ul>
+        </ul>
+      </form>
+
+      <button id="record" type="button">登録する</button>
+    
+      <section class="edit-list">
+        <div id="mask-edit" class="hidden"></div>
+    
+        <div id="modal-edit" class="hidden">
+          <h3>購入品リストを編集</h3>
+          <ul class="edit-menu">
+            <li id="menu-add">追加</li>
+            <li id="menu-edit">編集</li>
+          </ul>
+    
+          <section class="create-item hidden">
+            <form action="" method="post" id="add-form">
+              <label>品名 <input type="text" id="new-name" name="add-name"></label>
+              <label>価格 <input type="number" id="new-price" name="add-price"> 円</label>
+              <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
+            </form>
+            <button id="add-go">追加する</button>
+          </section>
+    
+          <section class="edit-items hidden">
+            <p>※ 購入履歴への反映は行われません。</p>
+            <form id="edit-from" action="" method="POST">
+            </form>
+            <ul>
+            </ul>
+            <button id="edit-go" type="button">編集する</button>
+          </section>
+          <div id="cansel">
+            <i class="fas fa-window-close"> キャンセル</i>
+          </div>
+        </div>
+      </section>
+    
+    </section>
+    
+    <section class="buying-history">
+      <h2><i class="fas fa-shopping-cart"></i> 購入履歴</h2>
+      <p>購入品の登録がありません。</p>
+      <table class="hidden">
+        <tbody>
+          <tr>
+            <th class="buying-date">購入日</th>
+            <th class="buying-name">品名</th>
+            <th class="buying-num">数</th>
+            <th class="buying-price">金額</th>
+            <th class="buying-delete"></th>
+          </tr>
+        </tbody>
+      </table>
+      <form action="" method="post" id="history-delete-form">
+        <input type="hidden" name="delete">
+      </form>
+    </section>
+
+    <!-- 購入品リストをjsonに変換してjsで扱う -->
+    <script>
+      const phpItems = <?php echo(json_encode($items)); ?>;
+      const phpPurchaseHistory = <?php echo(json_encode($purchaseItems)); ?>;
+    </script>
+
+    <?php if(isset($err)) : ?>
+    <script>alert('ERROR!');</script>
+    <?php endif; ?>
+
+    <footer>
+      &copy; ぽちっとかけいぼ
+    </footer>
+    <script src="https://www.gstatic.com/charts/loader.js"></script>
+    <script src="js/main.js"></script>
+  </body>
+
+</html>
